fb67079ee11e90dcdac96ed3797212b817a8660a
SAMIUL2022 <sami35bd@gmail.com>
2023-10-19 12:05:26 +0600

    added frequency array

--- V_Frequency_Array.c
+++ V_Frequency_Array.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 int main ()
 {
+int n;
+int m;
+scanf("%d %d",&n,&m);
+int a [n];
+
+int c [m+1];
+ for (int i = 1; i <= m; i++) {
+        c[i] = 0;}
+for (int i = 0; i < n; i++)
+{
+    scanf("%d",&a[i]);
+
+    c[a[i]]++;
+    
+}
+for (int i = 1; i <= m; i++)
+{
+  
+  
+     printf("%d\n",c[i]);
+  
+  
+}
 
 
 
